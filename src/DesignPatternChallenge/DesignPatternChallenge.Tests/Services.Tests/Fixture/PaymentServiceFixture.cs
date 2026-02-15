77e388f3aae9f4ac4553dd0712a54c68972c3cc0
Monica Lima <72664480+monica88lima@users.noreply.github.com>
2026-02-15 00:15:55 -0300

    test: implementa teste de unidade

---
+++ src/DesignPatternChallenge/DesignPatternChallenge.Tests/Services.Tests/Fixture/PaymentServiceFixture.cs
@@ -0,0 +1,36 @@
+﻿using DesignPatternChallenge.Interfaces;
+using DesignPatternChallenge.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternChallenge.Tests.Services.Tests.Fixture
+{
+    public class PaymentServiceFixture
+    {
+        public Mock<IPaymentFactory> FactoryMock { get; }
+        public Mock<IPaymentValidator> ValidatorMock { get; }
+        public Mock<IPaymentProcessor> ProcessorMock { get; }
+        public Mock<IPaymentLogger> LoggerMock { get; }
+        public PaymentService Service { get; }
+
+        public PaymentServiceFixture()
+        {
+            FactoryMock = new Mock<IPaymentFactory>();
+            ValidatorMock = new Mock<IPaymentValidator>();
+            ProcessorMock = new Mock<IPaymentProcessor>();
+            LoggerMock = new Mock<IPaymentLogger>();
+
+            FactoryMock.Setup(f => f.CreateValidator()).Returns(ValidatorMock.Object);
+            FactoryMock.Setup(f => f.CreateProcessor()).Returns(ProcessorMock.Object);
+            FactoryMock.Setup(f => f.CreateLogger()).Returns(LoggerMock.Object);
+
+            Service = new PaymentService(FactoryMock.Object);
+
+        }
+
+    }
+}
---
+++ src/DesignPatternChallenge/DesignPatternChallenge.Tests/Services.Tests/PaymentServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using DesignPatternChallenge.Interfaces;
+using DesignPatternChallenge.Tests.Services.Tests.Fixture;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DesignPatternChallenge.Tests.Services.Tests
+{
+    public  class PaymentServiceTests: IClassFixture<PaymentServiceFixture>
+    {
+        private readonly Fixture.PaymentServiceFixture _fixture;
+
+
+        public PaymentServiceTests(PaymentServiceFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+
+        [Fact]
+        public void ProcessPayment_InvalidCard_ShouldLogError()
+        {
+            // Arrange
+            _fixture.ValidatorMock.Setup(v => v.ValidateCard(It.IsAny<string>())).Returns(false);
+            _fixture.ProcessorMock.Setup(p => p.ProcessTransaction(It.IsAny<decimal>(), It.IsAny<string>()))
+                                 .Returns("Sucesso");
+            using var sw = new StringWriter();
+            Console.SetOut(sw);
+
+
+            // Act
+            _fixture.Service.ProcessPayment(20, "6");
+
+
+            // Assert
+            var output = sw.ToString().Trim();
+            Assert.Equal("Cartão inválido", output);
+        }
+
+        [Fact]
+        public void ProcessPayment_ValidCard_ShouldProcessTransaction()
+        {
+            // Arrange
+            _fixture.ValidatorMock.Setup(v => v.ValidateCard(It.IsAny<string>())).Returns(true);
+            _fixture.ProcessorMock.Setup(p => p.ProcessTransaction(It.IsAny<decimal>(), It.IsAny<string>()))
+                                  .Returns("Sucesso");
+
+            // Act
+            _fixture.Service.ProcessPayment(100, "123456");
+
+            // Assert
+            _fixture.LoggerMock.Verify(l => l.Log("Transação processada: Sucesso"), Times.Once);
+        }
+
+
+    }
+}
+     
